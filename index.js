93a755d78f86081a885ac70b36676ccb517b6676
Fabian A Ortiz Chavez <alonsofabianortiz@icloud.com>
2024-12-23 22:44:39 -0700

    Array Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+cats.lenght = 0
+function destructivelyAppendCat(Ralph){
+cats.splice("Milo", 0)
+cats.push("Ralph")
+}
+function destructivelyPrependCat(Bob){
+cats.unshift("Bob")
+}
+function destructivelyRemoveFirstCat(Milo){
+cats.splice(0)
+cats.push("Otis", "Garfield")
+}
+function destructivelyRemoveLastCat(Ralph, Bob){
+    cats.push("Milo", "Otis")
+    cats.splice(2, 4)
+ }
+function appendCat(Gato){
+    const cats = ["Milo", "Otis", "Garfield"]
+    const newCatsArray = [...cats, Gato]
+    return newCatsArray; 
+}
+function prependCat(Tom){
+    const cats = ["Milo", "Otis", "Garfield"]
+     return [Tom, ...cats]
+    
+}
+function removeLastCat(Gato){
+    const cats = ["Milo", "Otis", "Garfield"]
+    return cats.slice(0, -1)
+}
+function removeFirstCat(){
+    const cats =["Milo", "Otis", "Garfield"]
+    return cats.slice(1)
+}
